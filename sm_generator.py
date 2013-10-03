69fae2321b8d08eaf188d91da43d6b0479cbfe83
weslleymberg <weslleym.lisboa@gmail.com>
2013-10-03 18:18:32 -0300

    Implements states and transitions generator

--- sm_generator.py
+++ sm_generator.py
@@ -1,16 +1,27 @@
-from fluidity import StateMachine, state, transition
 
 class SMGenerator():
 
 	def __init__(self, data):
 		self.data = data
-		
-	def __repr__(self):
-		return "class Statemachine(StateMachine):"
-
-	def get_states(self):
-		list_states = []
+	
+	def _get_states(self):
+		print "\n"
 		for layer in self.data.layers:
 			for obj in layer.objects:
 				if obj.type.name == "UML - State":
-					print obj.properties["text"].value.text.strip()
+					print "\t\tstate('%s')" %(obj.properties["text"].value.text.strip())
+
+	def _get_transitions(self):
+		print "\n"
+		for layer in self.data.layers:
+			for obj in layer.objects:
+				if obj.type.name == "UML - Transition":
+					source = obj.handles[0].connected_to.object
+					target = obj.handles[1].connected_to.object
+					if source.type.name == "UML - State" and target.type.name == "UML - State":
+						print "\t\ttransition(from='%s', event='%s', to='%s')" %(source.properties["text"].value.text.strip(), obj.properties["trigger"].value, target.properties["text"].value.text.strip())
+
+	def generate(self):
+		print "from fluidity import StateMachine, state, transition\nclass MyStateMachine(StateMachine):"
+		self._get_states()
+		self._get_transitions()
