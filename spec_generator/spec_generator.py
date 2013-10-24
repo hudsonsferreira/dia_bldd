cb6c289489ca6190defaef934096e094e0da37ad
Hudson Ferreira <silvaferreira.hsf@gmail.com>
2013-10-24 18:47:36 -0200

    implemented the create_initial_state_testmethod

--- spec_generator/spec_generator.py
+++ spec_generator/spec_generator.py
@@ -1,8 +1,15 @@
-from statemachine_generator.sm_generator import SMGenerator
+from dia_bldd_modules.statemachine_generator.sm_generator import SMGenerator
 
-class SpecGenerator(SMGenerator):
+class SpecGenerator():
 
 	def __init__(self, data):
 		self.data = data
-		sm = SMGenerator()
-		sm.load_data(data)
+		self.sm = SMGenerator()
+		self.sm.load_data(data)
+
+	def create_initial_state_test(self):
+		initial_state = self.sm._get_initial_state()
+		phrase = 'initial_state |should_not| be_empty\n'+\
+		         'initial_state |should| be(1)\n'+\
+		         'initial_state |should| equal_to("%s")'%(initial_state)
+		return phrase 
