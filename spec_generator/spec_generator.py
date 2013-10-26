4c52c9ffa964d25477c113fbac9a6e40d6f086c6
Hudson Ferreira <silvaferreira.hsf@gmail.com>
2013-10-26 15:00:05 -0200

    changed string format to new pattern

--- spec_generator/spec_generator.py
+++ spec_generator/spec_generator.py
@@ -2,19 +2,19 @@ from dia_bldd_modules.statemachine_generator.sm_generator import SMGenerator
 
 class SpecGenerator():
 
-	def __init__(self, data):
-		self.data = data
-		self.sm = SMGenerator()
-		self.sm.load_data(data)
+    def __init__(self, data):
+        self.data = data
+        self.sm = SMGenerator()
+        self.sm.load_data(data)
 
-	def _header(self):
-		return 'impot unittest\n'+\
-			   'from should_dsl import should, should_not\n'+\
-			   'from dia_bldd_modules.statemachine_generator.StateMachine import MyStateMachine\n'
+    def _header(self):
+        return 'impot unittest\n'+\
+               'from should_dsl import should, should_not\n'+\
+               'from dia_bldd_modules.statemachine_generator.StateMachine import MyStateMachine\n'
 
-	def create_initial_state_test(self):
-		initial_state = self.sm._get_initial_state()
-		phrase = 'initial_state |should_not| be_empty\n'+\
-		         'initial_state |should| be(1)\n'+\
-		         'initial_state |should| equal_to("%s")'%(initial_state)
-		return phrase 
+    def create_initial_state_test(self):
+        initial_state = self.sm._get_initial_state()
+        phrase = 'initial_state |should_not| be_empty\n'+\
+                 'initial_state |should| be(1)\n'+\
+                 'initial_state |should| equal_to("{ini_state}")'.format(ini_state=initial_state)
+        return phrase
