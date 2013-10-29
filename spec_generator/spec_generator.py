7208b8ed8409e23911439aef6e8a80658b23cd92
Weslleymberg Lisboa <weslleym.lisboa@gmail.com>
2013-10-29 17:18:35 -0200

    Removes state machine code from test file

--- spec_generator/spec_generator.py
+++ spec_generator/spec_generator.py
@@ -7,18 +7,10 @@ class SpecGenerator():
         self.sm = SMGenerator()
         self.sm.load_data(data)
 
-    def _load_fluidity(self):
-        content = ''
-        fluidity = open('/home/hudson/.dia/python/dia_bldd_modules/statemachine_generator/StateMachine.py', 'r')
-        for line in fluidity.read():
-            content += line
-        return content
-
     def _header(self):
         return 'impot unittest\n'+\
                'from should_dsl import should\n'+\
                'from dia_bldd_modules.statemachine_generator.StateMachine import MyStateMachine\n\n'+\
-               self._load_fluidity() + '\n\n'+\
                'class FluidityTest(unittest.TestCase):\n\n'+\
                '\tdef setUp(self):\n'+\
                '\t\tself.machine = MyStateMachine()\n\n'
